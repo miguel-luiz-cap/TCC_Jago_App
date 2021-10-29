00d244d3c4be85400239a2cc42b08d69f3d72be0
Miguel Luiz Capristano <71992751+miguel-luiz-cap@users.noreply.github.com>
2021-10-28 22:44:42 -0300

    Correção nas cores e de algumas warings

--- src/RotasTab.js
+++ src/RotasTab.js
@@ -4,9 +4,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
 
-import Servicos from './pages/Servicos';
-import Menu from './StackNav';
-import Home from './pages/Home';
+import ServicosScreen from './pages/Servicos';
+import MenuScreen from './StackNav';
+import HomeScreen from './pages/Home';
 
 const Tab = createBottomTabNavigator();
 
@@ -14,10 +14,10 @@ const Tab = createBottomTabNavigator();
 export default function Rotas() {
   return (
     <Tab.Navigator
-      initialRouteName="Home"
-      
+      initialRouteName="Inicio-Tab"
+
       screenOptions={{
-        tabBarActiveTintColor: '#FF0',
+        tabBarActiveTintColor: '#FFF',
         tabBarInactiveTintColor: '#000',
         tabBarActiveBackgroundColor: '#87CEEB',
         "tabBarStyle": [
@@ -30,22 +30,10 @@ export default function Rotas() {
         //headerStyle: { backgroundColor: '#000', height: 80},
         //headerTintColor: '#FF0',
       }}>
-      <Tab.Screen
-        name="Inicio-Tab"
-        component={Home}
-        
-        options={{
-          title: 'Início',
-          headerShown: false,
-          tabBarLabel: 'Início',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="home" color={color} size={size} />
-          ),
-        }}
-      />
+
       <Tab.Screen
         name="Serviços-tab"
-        component={Servicos}
+        component={ServicosScreen}
         options={{
           tabBarLabel: 'Serviços',
           title: 'Serviços',
@@ -60,10 +48,22 @@ export default function Rotas() {
           ),
         }}
       />
-     
+      <Tab.Screen
+        name="Inicio-Tab"
+        component={HomeScreen}
+
+        options={{
+          title: 'Início',
+          headerShown: false,
+          tabBarLabel: 'Início',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
       <Tab.Screen
         name="Menu-tab"
-        component={Menu}
+        component={MenuScreen}
         options={{
           tabBarLabel: 'Menu',
           title: 'Menu',
--- src/StackNav.js
+++ src/StackNav.js
@@ -2,10 +2,10 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
-import Menu from './pages/Menu';
-import Sobre from './menuPages/Sobre';
-import Contato from './menuPages/Contato';
-import Login from './menuPages/Login';
+import MenuScreen from './pages/Menu';
+import SobreScreen from './menuPages/Sobre';
+import ContatoScreen from './menuPages/Contato';
+import LoginScreen from './menuPages/Login';
 
 const Stack = createStackNavigator();
 
@@ -16,10 +16,10 @@ export default function StackNav() {
         headerStyle: { backgroundColor: '#505' },
         headerTintColor: '#FF0',
     }}*/ >
-      <Stack.Screen name='Menu' component={Menu} options = {{headerShown: false }}/>
-      <Stack.Screen name='Sobre' component={Sobre} options = {{title: 'Sobre'}}/>
-      <Stack.Screen name='Contato' component={Contato} options = {{title: 'Contato'}}/>
-      <Stack.Screen name='Login' component={Login} options = {{title: 'Login'}}/>
+      <Stack.Screen name='Menu' component={MenuScreen} options = {{headerShown: false }}/>
+      <Stack.Screen name='Sobre' component={SobreScreen} options = {{title: 'Sobre'}}/>
+      <Stack.Screen name='Contato' component={ContatoScreen} options = {{title: 'Contato'}}/>
+      <Stack.Screen name='Login' component={LoginScreen} options = {{title: 'Login'}}/>
     </Stack.Navigator>
   )
 }
